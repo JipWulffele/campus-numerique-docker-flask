cf16bb6acbec6b142a791956de2e707f0e25eca4
jip <jipwulffele@gmail.com>
2025-11-18 11:13:17 +0100

    genre to Literal

--- src/models/image_description.py
+++ src/models/image_description.py
@@ -2,7 +2,16 @@ from pydantic import BaseModel, Field
 
 class ImageStroyTelling(BaseModel):
     background_color: str = Field(description="Background color of the image")
-    genre: str = Field(description="Genre of the image (e.g., fantasy, sci-fi, horror)")
+    genre: Literal[
+        "Fantasy",
+        "Sci-fi",
+        "Horror",
+        "Mystery",
+        "Adventure",
+        "Fairy-tale",
+        "Childeren's story",
+        "Post-apocalyptic"
+        ] = Field(description="Genre of the image")
     animal: str = Field(description="Type of animal in the image")
     num_animals: int = Field(description="Number of animals in the image")
     story: str = Field(description="A short story inspired by the image in the associated genre, maximum 100 words")
